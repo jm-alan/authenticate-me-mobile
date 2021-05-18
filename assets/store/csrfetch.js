47c102c61cf3d26942ab9448cc7622587e809875
josh <j.maxwell.alan@gmail.com>
2021-05-18 11:48:39 -0400

    rebuilding csrfetch

--- assets/store/csrfetch.js
+++ assets/store/csrfetch.js
@@ -1,52 +1,55 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { REACT_NATIVE_BASE_URL } from '@env';
 
+const setHeaders = async headers => {
+  headers['Content-Type'] || (headers['Content-Type'] = 'application/json');
+  headers['XSRF-Token'] = await AsyncStorage.getItem('token');
+};
+
+const wrappedFetch = async (url, ...args) => {
+  url = `${this.baseUrl}${url}?`;
+  const options = {};
+  if (
+    typeof args[0] === 'object' &&
+    !Array.isArray(args[0]) &&
+    args.length === 1
+  ) {
+    const queries = args[0];
+    for (const query in queries) url += `&${query}=${queries[query]}`;
+  } else if (
+    typeof args[0] === 'object' &&
+    Array.isArray(args[0]) &&
+    args.length === 1
+  ) throw new TypeError('GET request queries must be an object of the form { query: value }');
+  else if (args.length > 1);
+  return await (await window.fetch(url, options)).json();
+};
+
 export default {
   baseUrl: REACT_NATIVE_BASE_URL,
   async get (url, queries = {}) {
-    if (this === undefined || this === globalThis) {
-      throw new TypeError('Invalid context for invocation. Do not destructure csrfetch methods.');
-    }
     url = `${this.baseUrl}${url}?`;
-    for (const query in queries) {
-      url += `&${query}=${queries[query]}`;
-    }
+    for (const query in queries) url += `&${query}=${queries[query]}`;
     return await (await window.fetch(url)).json();
   },
   async post (url, body, headers = {}) {
-    if (this === undefined || this === globalThis) {
-      throw new TypeError('Invalid context for invocation. Do not destructure csrfetch methods.');
-    }
     url = `${this.baseUrl}${url}`;
-    headers['Content-Type'] || (headers['Content-Type'] = 'application/json');
-    headers['XSRF-Token'] = await AsyncStorage.getItem('token');
+    await setHeaders(headers);
     return await (await window.fetch(url, { method: 'POST', headers, body })).json();
   },
   async put (url, body, headers = {}) {
-    if (this === undefined || this === globalThis) {
-      throw new TypeError('Invalid context for invocation. Do not destructure csrfetch methods.');
-    }
     url = `${this.baseUrl}${url}`;
-    headers['Content-Type'] || (headers['Content-Type'] = 'application/json');
-    headers['XSRF-Token'] = await AsyncStorage.getItem('token');
+    await setHeaders(headers);
     return await (await window.fetch(url, { method: 'PUT', headers, body })).json();
   },
   async patch (url, body, headers = {}) {
-    if (this === undefined || this === globalThis) {
-      throw new TypeError('Invalid context for invocation. Do not destructure csrfetch methods.');
-    }
     url = `${this.baseUrl}${url}`;
-    headers['Content-Type'] || (headers['Content-Type'] = 'application/json');
-    headers['XSRF-Token'] = await AsyncStorage.getItem('token');
+    await setHeaders(headers);
     return await (await window.fetch(url, { method: 'PATCH', headers, body })).json();
   },
   async delete (url, body, headers = {}) {
-    if (this === undefined || this === globalThis) {
-      throw new TypeError('Invalid context for invocation. Do not destructure csrfetch methods.');
-    }
     url = `${this.baseUrl}${url}`;
-    headers['Content-Type'] || (headers['Content-Type'] = 'application/json');
-    headers['XSRF-Token'] = await AsyncStorage.getItem('token');
+    await setHeaders(headers);
     return await (await window.fetch(url, { method: 'POST', headers, body })).json();
   }
 };
